f1fb0f20bab0ec2dd7b133d5ee004a9aa5aa74e3
test_who <test@test>
2020-01-19 13:17:04 +0900

    calculate + and -

--- 9cc.c
+++ 9cc.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *input;
-
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
   
-  input = argv[1];
+  char *p = argv[1];
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(input));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
--- test.sh
+++ test.sh
@@ -16,7 +16,11 @@ try() {
   fi
 }
 
+make
 try 0 0
-try 42 42
+try "42" "42"
+try 42 "40+2"
+try 0 '100-100'
+try 10 '100-100+10'
 
 echo OK
