abe6cb876f3b42c842eef709d283186878891b87
test_who <test@test>
2020-01-19 12:49:50 +0900

    something

--- 9cc.c
+++ 9cc.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *input;
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
+  
+  input = argv[1];
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %d\n", atoi(input));
   printf("  ret\n");
   return 0;
 }
