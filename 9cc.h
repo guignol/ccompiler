bb91a603ad0af2f83bad640b4d4e14af8333ac74
test_who <test@test>
2020-01-20 19:01:02 +0900

    file division 3

--- 9cc.h
+++ 9cc.h
@@ -5,6 +5,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+// 入力プログラム
+extern char *user_input;
+
+// トークンの種類
+typedef enum
+{
+  TK_RESERVED, // 記号
+  TK_NUM,      // 整数トークン
+  TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token
+{
+  TokenKind kind; // トークンの型
+  Token *next;    // 次の入力トークン
+  int val;        // kindがTK_NUMの場合、その数値
+  char *str;      // トークン文字列
+  int len;        // トークンの長さ
+};
+
+// 現在着目しているトークン
+extern Token *token;
+
+/////////////////////////////////////////////////
+
 // 抽象構文木のノードの種類
 typedef enum
 {
@@ -30,4 +58,5 @@ struct Node
   int val;       // kindがND_NUMの場合のみ使う
 };
 
+Node *expr();
 void gen(Node *node);
---
+++ parse.c
@@ -0,0 +1,177 @@
+#include "9cc.h"
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, ""); // pos個の空白を出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// 次のトークンが期待している記号のときには、トークンを読み進めて
+// 真を返す。それ以外の場合には偽を返す。
+bool consume(char *op)
+{
+  if (token->kind != TK_RESERVED ||
+      strlen(op) != token->len ||
+      memcmp(token->str, op, token->len))
+    return false;
+  token = token->next;
+  return true;
+}
+
+// 次のトークンが期待している記号のときには、トークンを1つ読み進める。
+// それ以外の場合にはエラーを報告する。
+void expect(char *op)
+{
+  if (token->kind != TK_RESERVED ||
+      strlen(op) != token->len ||
+      memcmp(token->str, op, token->len))
+    error_at(token->str, "'%s'ではありません", op);
+  token = token->next;
+}
+
+// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
+// それ以外の場合にはエラーを報告する。
+int expect_number()
+{
+  if (token->kind != TK_NUM)
+    error_at(token->str, "数ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+//////////////////////////////////////////////////////////////////
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+// expr       = equality
+// equality   = relational ("==" relational | "!=" relational)*
+// relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+// add        = mul ("+" mul | "-" mul)*
+// mul        = unary ("*" unary | "/" unary)*
+// unary      = ("+" | "-")? primary
+// primary    = num | "(" expr ")"
+
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+
+Node *expr()
+{
+  return equality();
+}
+
+Node *equality()
+{
+  Node *node = relational();
+
+  for (;;)
+  {
+    if (consume("=="))
+      node = new_node(ND_EQL, node, relational());
+    else if (consume("!="))
+      node = new_node(ND_NOT, node, relational());
+    else
+      return node;
+  }
+}
+
+Node *relational()
+{
+  Node *node = add();
+
+  for (;;)
+  {
+    if (consume("<"))
+      node = new_node(ND_LESS, node, add());
+    else if (consume(">"))
+      node = new_node(ND_LESS, add(), node);
+    else if (consume("<="))
+      node = new_node(ND_LESS_EQL, node, add());
+    else if (consume(">="))
+      node = new_node(ND_LESS_EQL, add(), node);
+    else
+      return node;
+  }
+}
+
+Node *add()
+{
+  Node *node = mul();
+
+  for (;;)
+  {
+    if (consume("+"))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume("-"))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul()
+{
+  Node *node = unary();
+
+  for (;;)
+  {
+    if (consume("*"))
+      node = new_node(ND_MUL, node, unary());
+    else if (consume("/"))
+      node = new_node(ND_DIV, node, unary());
+    else
+      return node;
+  }
+}
+
+Node *unary()
+{
+  if (consume("+"))
+    return primary();
+  if (consume("-"))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
+}
+
+Node *primary()
+{
+  // 次のトークンが"("なら、"(" expr ")"のはず
+  if (consume("("))
+  {
+    Node *node = expr();
+    expect(")");
+    return node;
+  }
+
+  // そうでなければ数値のはず
+  return new_node_num(expect_number());
+}
