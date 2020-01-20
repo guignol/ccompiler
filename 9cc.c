38a75a7cdbfc500dab299d2f85e711509d23519a
test_who <test@test>
2020-01-20 16:42:23 +0900

    unary

--- 9cc.c
+++ 9cc.c
@@ -175,10 +175,12 @@ Node *new_node_num(int val) {
 }
 
 // expr    = mul ("+" mul | "-" mul)*
-// mul     = primary ("*" primary | "/" primary)*
+// mul     = unary ("*" unary | "/" unary)*
+// unary   = ("+" | "-")? primary
 // primary = num | "(" expr ")"
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *primary() {
@@ -193,15 +195,23 @@ Node *primary() {
   return new_node_num(expect_number());
 }
 
+Node *unary() {
+  if (consume('+'))
+    return primary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
+}
+
 Node *mul()
 {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
