244d5e6676bd23d5523f22dcf22acee8b524a4e9
test_who <test@test>
2020-01-20 18:50:00 +0900

    file division 2

--- 9cc.h
+++ 9cc.h
@@ -5,3 +5,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+// 抽象構文木のノードの種類
+typedef enum
+{
+  ND_ADD,      // +
+  ND_SUB,      // -
+  ND_MUL,      // *
+  ND_DIV,      // /
+  ND_EQL,      // ==
+  ND_NOT,      // !=
+  ND_LESS,     // <
+  ND_LESS_EQL, // <=
+  ND_NUM,      // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node
+{
+  NodeKind kind; // ノードの型
+  Node *lhs;     // 左辺
+  Node *rhs;     // 右辺
+  int val;       // kindがND_NUMの場合のみ使う
+};
+
+void gen(Node *node);
--- main.c
+++ main.c
@@ -156,31 +156,6 @@ Token *tokenize()
   return head.next;
 }
 
-// 抽象構文木のノードの種類
-typedef enum
-{
-  ND_ADD,      // +
-  ND_SUB,      // -
-  ND_MUL,      // *
-  ND_DIV,      // /
-  ND_EQL,      // ==
-  ND_NOT,      // !=
-  ND_LESS,     // <
-  ND_LESS_EQL, // <=
-  ND_NUM,      // 整数
-} NodeKind;
-
-typedef struct Node Node;
-
-// 抽象構文木のノードの型
-struct Node
-{
-  NodeKind kind; // ノードの型
-  Node *lhs;     // 左辺
-  Node *rhs;     // 右辺
-  int val;       // kindがND_NUMの場合のみ使う
-};
-
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
 {
   Node *node = calloc(1, sizeof(Node));
@@ -304,60 +279,6 @@ Node *primary()
 
   // そうでなければ数値のはず
   return new_node_num(expect_number());
-}
-
-void gen(Node *node)
-{
-  if (node->kind == ND_NUM)
-  {
-    printf("  push %d\n", node->val);
-    return;
-  }
-
-  gen(node->lhs);
-  gen(node->rhs);
-
-  printf("  pop rdi\n");
-  printf("  pop rax\n");
-
-  switch (node->kind)
-  {
-  case ND_ADD:
-    printf("  add rax, rdi\n");
-    break;
-  case ND_SUB:
-    printf("  sub rax, rdi\n");
-    break;
-  case ND_MUL:
-    printf("  imul rax, rdi\n");
-    break;
-  case ND_DIV:
-    printf("  cqo\n");
-    printf("  idiv rdi\n");
-    break;
-  case ND_EQL:
-    printf("  cmp rax, rdi\n");
-    printf("  sete al\n");
-    printf("  movzb rax, al\n");
-    break;
-  case ND_NOT:
-    printf("  cmp rax, rdi\n");
-    printf("  setne al\n");
-    printf("  movzb rax, al\n");
-    break;
-  case ND_LESS:
-    printf("  cmp rax, rdi\n");
-    printf("  setl al\n");
-    printf("  movzb rax, al\n");
-    break;
-  case ND_LESS_EQL:
-    printf("  cmp rax, rdi\n");
-    printf("  setle al\n");
-    printf("  movzb rax, al\n");
-    break;
-  }
-
-  printf("  push rax\n");
 }
 
 int main(int argc, char **argv)
